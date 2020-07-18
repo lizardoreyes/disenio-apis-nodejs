55ef79ba1d1d6f2e161b20cf23698e01059bf96a
Lizardo Reyes <ciberutilidades@gmail.com>
2020-07-17 19:45:46 -0500

    Validacion de los parametros enviados a las rutas

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Diseño de una API Rest usando Node.js y Express para ser consumido por cualquier tipo de aplicación, ya se web, mobile o desktop.
 
 ## Versión
-0.0.3
+0.0.4
 
 ## Características:
 
 - Creación del servidor usando Nodejs y Express.
 - CRUD a un [Array] como base de datos.
+- Validación de parametros enviandos a las rutas.
--- api/resources/products/products.route.js
+++ api/resources/products/products.route.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const uuid = require('uuid').v4;
 const _ = require("underscore")
+const Joi = require('@hapi/joi');
 const { products } = require('./../../../database');
 const productsRouter = express.Router();
+
+const blueprintProduct = Joi.object({
+    title: Joi.string().max(100).required(),
+    price: Joi.number().positive().precision(2).required(),
+    currency: Joi.string().length(3).uppercase()
+})
+
+const validateProduct = (req, res, next) => {
+    let result = blueprintProduct.validate(req.body, { abortEarly: false, convert: false })
+    if(result.error === undefined) {
+        next()
+    } else {
+        const errors = result.error.details.reduce((acc, err) => acc + `[${err.message}]`, "")
+        res.status(400).send(`El producto en el body debe especificar titulo, precio y moneda. Errores en tu request: ${errors}`)
+    }
+}
 
 productsRouter.get("/", (req, res) => {
     res.json(products)
 })
 
-productsRouter.post("/", (req, res) => {
+productsRouter.post("/", validateProduct, (req, res) => {
     const newProduct = {id: uuid(), ...req.body}
-    if(!newProduct.title || !newProduct.price || !newProduct.currency) {
-        // Bad Request
-        res.status(400).send(`Tu producto debe especificar un titulo, precio y moneda`)
-        return
-    }
+
     products.push(newProduct)
     res.status(201).json(newProduct)
 })
@@ -29,13 +42,7 @@ productsRouter.get("/:id", (req, res) => {
 })
 
 // Reemplazamos totalmente un producto
-productsRouter.put("/:id", (req, res) => {
-    if(!req.body.title || !req.body.price || !req.body.currency) {
-        // Bad Request
-        res.status(400).send(`Tu producto debe especificar un titulo, precio y moneda`)
-        return
-    }
-
+productsRouter.put("/:id", validateProduct ,(req, res) => {
     const pos = products.findIndex(product => product.id === req.params.id)
     if (pos === -1) {
         res.status(404).send(`El producto con id [${req.params.id}] no existe`)
