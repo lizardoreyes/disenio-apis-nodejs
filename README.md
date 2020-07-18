743e50fde6370366872a879b77a68eb31c7be8e5
Lizardo Reyes <ciberutilidades@gmail.com>
2020-07-17 20:47:10 -0500

    Instalando morgan e integrandolo con winston

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 Diseño de una API Rest usando Node.js y Express para ser consumido por cualquier tipo de aplicación, ya se web, mobile o desktop.
 
 ## Versión
-0.0.6
+0.0.7
 
 ## Características:
 
 - Creación del servidor usando Nodejs y Express.
 - CRUD a un [Array] como base de datos.
 - Validación de parametros enviandos a las rutas.
-- Uso de Winston para el registro de logs
+- Uso de Winston para el registro de logs.
+- Integracion de morgan con winston para crear logs de los requests.
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const bodyParser = require("body-parser");
 const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan")
 const productsRouter = require('./api/resources/products/products.route');
 const logger = require('./utils/logger');
 
-
+app.use(morgan("short", {
+    stream: {
+        write: message => logger.info(message.trim())
+    }
+}))
 app.use(bodyParser.json())
 
 app.use("/products", productsRouter)
-
-
 app.get("/", (req, res) => {
     res.send("API funcionando")
 })
